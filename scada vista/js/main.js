cf8f1b6c587dbab05aea6d991d19d808564a7f58
SHERCAN <sidereoyess@gmail.com>
2022-12-26 11:34:31 -0500

    ubicación

--- scada vista/js/main.js
+++ scada vista/js/main.js
@@ -11,66 +11,61 @@
   spinner();
 
   // Back to top button
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-      $(".back-to-top").fadeIn("slow");
-    } else {
-      $(".back-to-top").fadeOut("slow");
-    }
-  });
-  $(".back-to-top").click(function () {
-    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
-    return false;
-  });
+  // $(window).scroll(function () {
+  //   if ($(this).scrollTop() > 300) {
+  //     $(".back-to-top").fadeIn("slow");
+  //   } else {
+  //     $(".back-to-top").fadeOut("slow");
+  //   }
+  // });
+  // $(".back-to-top").click(function () {
+  //   $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+  //   return false;
+  // });
 
-  // Sidebar Toggler
-  $(".sidebar-toggler").click(function () {
-    $(".sidebar, .content").toggleClass("open");
-    return false;
-  });
+  // // Sidebar Toggler
+  // $(".sidebar-toggler").click(function () {
+  //   $(".sidebar, .content").toggleClass("open");
+  //   return false;
+  // });
 
-  // Progress Bar
-  $(".pg-bar").waypoint(
-    function () {
-      $(".progress .progress-bar").each(function () {
-        $(this).css("width", $(this).attr("aria-valuenow") + "%");
-      });
-    },
-    { offset: "80%" }
-  );
+  // // Progress Bar
+  // $(".pg-bar").waypoint(
+  //   function () {
+  //     $(".progress .progress-bar").each(function () {
+  //       $(this).css("width", $(this).attr("aria-valuenow") + "%");
+  //     });
+  //   },
+  //   { offset: "80%" }
+  // );
 
   // // Chart Global Color
   // // Chart.defaults.color = "#6C7293";
   // // Chart.defaults.borderColor = "#000000";
 
   // // Worldwide Sales Chart
-  var ctx1 = $("#worldwide-sales").get(0).getContext("2d");
-  var myChart1 = new Chart(ctx1, {
-    type: "bar",
-    data: {
-      labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
-      datasets: [
-        {
-          label: "USA",
-          data: [15, 30, 55, 65, 60, 80, 95],
-          backgroundColor: "rgba(235, 22, 22, .7)",
-        },
-        {
-          label: "UK",
-          data: [8, 35, 40, 60, 70, 55, 75],
-          backgroundColor: "rgba(235, 22, 22, .5)",
-        },
-        {
-          label: "AU",
-          data: [12, 25, 45, 55, 65, 70, 60],
-          backgroundColor: "rgba(235, 22, 22, .3)",
-        },
-      ],
-    },
-    options: {
-      responsive: true,
-    },
-  });
+  // var ctx1 = $("#worldwide-sales").get(0).getContext("2d");
+  // var myChart1 = new Chart(ctx1, {
+  //   type: "bar",
+  //   data: {
+  //     labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+  //     datasets: [
+  //       {
+  //         label: "USA",
+  //         data: [15, 30, 55, 65, 60, 80, 95],
+  //         backgroundColor: "rgba(235, 22, 22, .7)",
+  //       },
+  //       {
+  //         label: "UK",
+  //         data: [8, 35, 40, 60, 70, 55, 75],
+  //         backgroundColor: "rgba(235, 22, 22, .5)",
+  //       },
+  //     ],
+  //   },
+  //   options: {
+  //     responsive: true,
+  //   },
+  // });
 
   // // Salse & Revenue Chart
   // var ctx2 = $("#salse-revenue").get(0).getContext("2d");
@@ -99,22 +94,22 @@
   // });
 
   // Single Line Chart
-  var ctx3 = $("#line-chart").get(0).getContext("2d");
-  var myChart3 = new Chart(ctx3, {
-    type: "line",
-    data: {
-      labels: [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150],
-      datasets: [
-        {
-          label: "Salse",
-          fill: false,
-          backgroundColor: "rgba(235, 22, 22, .7)",
-          data: [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15],
-        },
-      ],
-    },
-    options: {
-      responsive: true,
-    },
-  });
+  // var ctx3 = $("#line-chart").get(0).getContext("2d");
+  // var myChart3 = new Chart(ctx3, {
+  //   type: "line",
+  //   data: {
+  //     labels: [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150],
+  //     datasets: [
+  //       {
+  //         label: "Salse",
+  //         fill: false,
+  //         backgroundColor: "rgba(235, 22, 22, .7)",
+  //         data: [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15],
+  //       },
+  //     ],
+  //   },
+  //   options: {
+  //     responsive: true,
+  //   },
+  // });
 })(jQuery);
